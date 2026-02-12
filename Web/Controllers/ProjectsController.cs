6e7303d06f344e5cb3464704c631dd5eb8bc0433
Stepan Demchenko <ese4@yandex.ru>
2026-02-12 16:17:32 +0700

    CRUD для проектов

--- Web/Controllers/ProjectsController.cs
+++ Web/Controllers/ProjectsController.cs
@@ -13,11 +13,7 @@ namespace Web.Controllers
             _projectService = projectService;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var projects = await _projectService.GetAllProjectsAsync();
-            return View(projects);
-        }
+        
 
         public IActionResult Create()
         {
@@ -31,7 +27,61 @@ namespace Web.Controllers
                 return View(project);
 
             await _projectService.CreateProjectAsync(project);
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index","Home");
         }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var project = await _projectService.GetProjectAsync(id);
+            if (project == null)
+                return NotFound();
+
+            return View(project);
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var project = await _projectService.GetProjectAsync(id);
+            if (project == null)
+                return NotFound();
+
+            return View(project);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid id, Project project)
+        {
+            if (!ModelState.IsValid)
+                return View(project);
+
+            var existingProject = await _projectService.GetProjectAsync(id);
+            if (existingProject == null)
+                return NotFound();
+
+            // Обновляем только нужные поля
+            existingProject.Name = project.Name;
+            existingProject.Description = project.Description;
+
+            await _projectService.UpdateProjectAsync(existingProject);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var project = await _projectService.GetProjectAsync(id);
+            if (project == null)
+                return NotFound();
+
+            return View(project);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _projectService.DeleteProjectAsync(id);
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
