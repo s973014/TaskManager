65812cea95e7c791fca88feb426a0985dbc03c9a
Stepan Demchenko <ese4@yandex.ru>
2026-02-12 12:50:33 +0700

    Создание AppDbContext

--- Infrastructure/Data/AppDBContext.cs
+++ Infrastructure/Data/AppDBContext.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Microsoft.EntityFrameworkCore;
+using Domain.Common;
+using Domain.Entities;
 
 
 namespace Infrastructure.Data
 {
-    internal class AppDBContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) {
+
+        
+        }
+
+        public DbSet<Project> Projects => Set<Project>();
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+        public override int SaveChanges()
+        {
+            UpdateTimeStamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            UpdateTimeStamps();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        public void UpdateTimeStamps()
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdateTimestamp();
+                }
+            }
+        }
+
     }
 }
