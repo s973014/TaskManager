9c53c12b879f3f9bbd54e329c02f48524b9fcafc
Stepan Demchenko <ese4@yandex.ru>
2026-02-12 12:55:38 +0700

    Создание конфигураций сущностей

---
+++ Infrastructure/Data/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("projects");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id)
+                   .HasColumnName("id");
+
+            builder.Property(p => p.Name)
+                   .HasColumnName("name")
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(p => p.Description)
+                   .HasColumnName("description")
+                   .HasMaxLength(1000);
+
+            builder.Property(p => p.CreatedAt)
+                   .HasColumnName("created_at")
+                   .IsRequired();
+
+            builder.Property(p => p.UpdatedAt)
+                   .HasColumnName("updated_at");
+
+            builder.HasMany(p => p.Tasks)
+                   .WithOne(t => t.Project)
+                   .HasForeignKey(t => t.ProjectId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Infrastructure/Data/Configurations/TaskItemConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class TaskItemConfiguration : IEntityTypeConfiguration<TaskItem>
+    {
+        public void Configure(EntityTypeBuilder<TaskItem> builder)
+        {
+            builder.ToTable("tasks");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Id)
+                   .HasColumnName("id");
+
+            builder.Property(t => t.Title)
+                   .HasColumnName("title")
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(t => t.Description)
+                   .HasColumnName("description")
+                   .HasMaxLength(2000);
+
+            builder.Property(t => t.Status)
+                   .HasColumnName("status")
+                   .IsRequired();
+
+            builder.Property(t => t.Priority)
+                   .HasColumnName("priority")
+                   .IsRequired();
+
+            builder.Property(t => t.DueDate)
+                   .HasColumnName("due_date");
+
+            builder.Property(t => t.ProjectId)
+                   .HasColumnName("project_id")
+                   .IsRequired();
+
+            builder.Property(t => t.CreatedAt)
+                   .HasColumnName("created_at")
+                   .IsRequired();
+
+            builder.Property(t => t.UpdatedAt)
+                   .HasColumnName("updated_at");
+
+            builder.HasIndex(t => t.ProjectId);
+            builder.HasIndex(t => t.Status);
+        }
+    }
+}
