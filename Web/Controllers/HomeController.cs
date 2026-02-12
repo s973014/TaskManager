aa0ed0110f9aba4712daddd7e6d4c4fdac546639
Stepan Demchenko <ese4@yandex.ru>
2026-02-12 15:04:28 +0700

    Создание главной страницы и страницы проектов

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Application.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Web.ViewModels;
@@ -6,21 +7,17 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProjectService _projectService;
+
+        public HomeController(IProjectService projectService)
         {
-            return View();
+            _projectService = projectService;
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-            
+            var recentProjects = await _projectService.GetAllProjectsAsync();
+            return View(recentProjects);
         }
     }
 }
---
+++ Web/Controllers/ProjectsController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var projects = await _projectService.GetAllProjectsAsync();
+            return View(projects);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Project project)
+        {
+            if (!ModelState.IsValid)
+                return View(project);
+
+            await _projectService.CreateProjectAsync(project);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
